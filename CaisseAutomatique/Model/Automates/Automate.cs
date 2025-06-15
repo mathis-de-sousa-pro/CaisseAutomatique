e22a0eca23f393d49ab3b5a9cf2c637c2f14ec9e
MathisDeSousa <157818653+mathis-de-sousa-pro@users.noreply.github.com>
2025-06-15 13:34:48 +0200

    Setup basic automate infrastructure with message binding

---
+++ CaisseAutomatique/Model/Automates/Automate.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CaisseAutomatique.Model.Automates
+{
+    /// <summary>
+    /// Automate gérant les états de la caisse automatique
+    /// </summary>
+    public class Automate : INotifyPropertyChanged
+    {
+        private Etat etatCourant;
+        public Etat EtatCourant { get => etatCourant; private set => etatCourant = value; }
+
+        /// <summary>
+        /// Message actuel de la caisse (donné par l'état courant)
+        /// </summary>
+        public string Message => this.etatCourant.Message;
+
+        public Automate()
+        {
+            this.etatCourant = new Etats.EtatAttenteClient(this);
+        }
+
+        /// <summary>
+        /// Active l'automate avec un évènement
+        /// </summary>
+        /// <param name="evt">évènement reçu</param>
+        public void Activer(Evenement evt)
+        {
+            this.etatCourant.Action(evt);
+            this.etatCourant = this.etatCourant.Transition(evt);
+            NotifyPropertyChanged(nameof(Message));
+        }
+
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
